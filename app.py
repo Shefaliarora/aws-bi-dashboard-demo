6bfd7ebc6b0941345c6ea948d35d0ecc08400e44
Shefali Arora <41606902+Shefaliarora@users.noreply.github.com>
2025-06-11 08:34:25 +0530

    Update app.py

--- app.py
+++ app.py
@@ -15,7 +15,7 @@ df = pd.DataFrame(data)
 app = dash.Dash(__name__)
 
 app.layout = html.Div([
-    html.H1("📊 Super Interactive BI Dashboard v1", style={"textAlign": "center"}),
+    html.H1("📊 Super Interactive BI Dashboard v2", style={"textAlign": "center"}),
 
     html.Label("Select Department:"),
     dcc.Dropdown(
